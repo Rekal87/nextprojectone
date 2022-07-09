0e25bfc6cd12439b4f61af13a9c98db09cb31ba8
Nnamdi <nnamdi.b@gmail.com>
2022-07-09 05:18:08 +0200

    added a filter check for searches

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,7 +1,44 @@
+import { useRouter } from 'next/router';
+import { getFilteredEvents } from '../../dummy-data';
+import EventList from '../../components/events/event-list';
+
 export default function FilteredEventsPage() {
+  const router = useRouter();
+
+  const filterData = router.query.slug;
+
+  // to not get a error on first load due to react undefined, this prevents crash
+  if (!filterData) {
+    return <p className='center'>Loading...</p>;
+  }
+
+  const filetedYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  // converting the array strings into a number
+  const numYear = +filetedYear;
+  const numMonth = +filteredMonth;
+
+  // checking if the filter is valid
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid filter. Please adjust your values</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({ year: numYear, month: numMonth });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No events found for the chosen filter</p>;
+  }
+
   return (
     <div>
-      <h1>Filtered Events</h1>
+      <EventList items={filteredEvents} />
     </div>
   );
 }
