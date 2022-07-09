5b6726d73790c95452891fff3b979058e8f25877
Nnamdi <nnamdi.b@gmail.com>
2022-07-09 04:24:17 +0200

    added search components

--- components/ui/button.js
+++ components/ui/button.js
@@ -2,9 +2,18 @@ import Link from 'next/link';
 import styles from './button.module.css';
 
 export default function Button(props) {
+  // button checks if a link props has been passed, if truthy will render a link
+  // if not it will render a normal button
+  if (props.link) {
+    return (
+      <Link href={props.link}>
+        <a className={styles.btn}>{props.children}</a>
+      </Link>
+    );
+  }
   return (
-    <Link href={props.link}>
-      <a className={styles.btn}>{props.children}</a>
-    </Link>
+    <button className={styles.btn} onClick={props.onClick}>
+      {props.children}
+    </button>
   );
 }
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,15 @@
 import EventList from '../../components/events/event-list';
 import { getAllEvents } from '../../dummy-data';
+import EventsSearch from '../../components/events/events-search';
+import { Fragment } from 'react';
 
 export default function AllEventPage() {
   const allEvents = getAllEvents();
 
   return (
-    <div>
+    <Fragment>
+      <EventsSearch />
       <EventList items={allEvents} />
-    </div>
+    </Fragment>
   );
 }
