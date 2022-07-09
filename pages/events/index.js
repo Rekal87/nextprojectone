2e3b8e99a5dc901523b0add07a02cc8bc8275ee9
Nnamdi <nnamdi.b@gmail.com>
2022-07-09 04:52:40 +0200

    added onClick event for saerch and useRef

--- pages/events/index.js
+++ pages/events/index.js
@@ -2,13 +2,20 @@ import EventList from '../../components/events/event-list';
 import { getAllEvents } from '../../dummy-data';
 import EventsSearch from '../../components/events/events-search';
 import { Fragment } from 'react';
+import { useRouter } from 'next/dist/client/router';
 
 export default function AllEventPage() {
   const allEvents = getAllEvents();
+  const router = useRouter();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
 
   return (
     <Fragment>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={allEvents} />
     </Fragment>
   );
