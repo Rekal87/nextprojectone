6eeb665249b12937b9678681c20e487505054426
Nnamdi <nnamdi.b@gmail.com>
2022-07-16 21:52:33 +0200

    adding fetch from firebase database

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,19 @@
-import { getFeaturedEvents } from '../dummy-data';
+import { getFeaturedEvents } from '../helpers/api-util';
 import EventList from '../components/events/event-list';
 
-export default function Home() {
-  const featuredEvents = getFeaturedEvents();
-
+export default function Home(props) {
   return (
     <div>
-      <EventList items={featuredEvents} />
+      <EventList items={props.events} />
     </div>
   );
 }
 
 export async function getStaticProps() {
-  return fetch(
-    'https://nextjs-course-9b48b-default-rtdb.europe-west1.firebasedatabase.app/'
-  ).then((response) => console.log(response));
+  const featuredEvents = await getFeaturedEvents();
+  return {
+    props: {
+      events: featuredEvents,
+    },
+  };
 }
